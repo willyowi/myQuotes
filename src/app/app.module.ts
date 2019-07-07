8b0a0fa2f381b9ff4de60e3024417befb3c4de99
willyowi <wilsonowino1@gmail.com>
2019-07-08 00:21:35 +0300

    generate  quote detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
@@ -18,7 +18,9 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/votes/votes.component.ts
+++ src/app/votes/votes.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
-  selector: 'app-vote',
-  templateUrl: './vote.component.html',
-  styleUrls: ['./vote.component.css']
+  selector: 'app-votes',
+  templateUrl: './votes.component.html',
+  styleUrls: ['./votes.component.css']
 })
-export class VoteComponent implements OnInit {
+export class VotesComponent implements OnInit {
 
   @Output() isComplete = new EventEmitter<boolean>();//listens to button click to display results
 //starting point for votes in two variables with 0 as default
