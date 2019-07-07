bab635ed0f4cab4e8c2c5b1e360aa1aa731075ff
willyowi <wilsonowino1@gmail.com>
2019-07-08 01:28:12 +0300

    add quote 5

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'Kevin heart', 'I DON’T have EX’s! I have Y’s. Like ‘Y the hell did I date you?!’',new Date(2018,6,14)),
+    new Quote(1, 'Kevin heart', '',new Date(2018,6,14)),
     new Quote(2,'Amy Poehler','Take your risks now, as you grow older you become more fearful and less flexible. And I mean that literally. I hurt my knee this week on the treadmill, and it wasn’t even on.',new Date(2019,4,9)),
     new Quote(3,'Vin Diesel','The trouble with the rat race is that even if you win, you’re still a rat.',new Date(2019,6,1)),
     new Quote(4,'Chris Rock','Accept who you are. Unless you are a serial killer',new Date(2019,3,23)),
     new Quote(5,'Trevor Noah','Does America really need to the best at everything? You already dominate the world in economics, military power and obesity.',new Date(2019,1,21)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2018,8,6)),
+    new Quote(6,'Trevor Noah','Don’t worry about the world coming to an end today coz guess what, It’s already tomorrow in Australia.',new Date(2018,8,6)),
   ];
   toggleDetails(index){
    this.quotes[index].showDescription = !this.quotes[index].showDescription;
