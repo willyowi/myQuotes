f7fbc67a33430f497b3459d1b1130f993d03937e
willyowi <wilsonowino1@gmail.com>
2019-07-08 14:56:09 +0300

    change in qote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,10 @@
 export class Quote {
     public showDescription: boolean;
-    constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+    constructor(public id: number,
+        public name: string,
+        public description: string,
+        public submitter: string,
+        public completeDate: Date){
       this.showDescription=false;
     }
   }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'Trevor Noah', 'Don’t worry about the world coming to an end today coz guess what, It’s already tomorrow in Australia.',new Date(2018,6,14)),
-    new Quote(2,'Amy Poehler','If People are talking behind your back, jus fart',new Date(2019,4,9)),
-    new Quote(3,'Vin Diesel','Dont Break someones heart,they only have one, break their bones,they have 206 of them',new Date(2019,6,1)),
-    new Quote(4,'Chris Rock','Accept who you are. Unless you are a serial killer',new Date(2019,3,23)),
-    new Quote(5,'Trevor Noah','Does America really need to the best at everything? You already dominate the world in economics, military power and obesity.',new Date(2019,1,21)),
-    new Quote(6,'Kevin Hart','I DON’T have EX’s! I have Y’s. Like ‘Y the hell did I date you?!',new Date(2018,8,6)),
+    new Quote(1, 'Trevor Noah', 'Don’t worry about the world coming to an end today coz guess what, It’s already tomorrow in Australia.','This Quote was submitted by',new Date(2018,6,14)),
+    new Quote(2,'Amy Poehler','If People are talking behind your back, jus fart','This Quote was submitted by',new Date(2019,4,9)),
+    new Quote(3,'Vin Diesel','Dont Break someones heart,they only have one, break their bones,they have 206 of them','This Quote was submitted by',new Date(2019,6,1)),
+    new Quote(4,'Chris Rock','Accept who you are. Unless you are a serial killer','This Quote was submitted by',new Date(2019,3,23)),
+    new Quote(5,'Trevor Noah','Does America really need to the best at everything? You already dominate the world in economics, military power and obesity.','This Quote was submitted by',new Date(2019,1,21)),
+    new Quote(6,'Kevin Hart','I DON’T have EX’s! I have Y’s. Like ‘Y the hell did I date you?!','This Quote was submitted by',new Date(2018,8,6)),
   ];
   toggleDetails(index){
    this.quotes[index].showDescription = !this.quotes[index].showDescription;
